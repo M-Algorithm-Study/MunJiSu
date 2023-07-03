b1aac3c7408287dd3b0e44ebf9d3c9824a3a66e9
JiSuMun <mjs3827@naver.com>
2023-07-04 03:44:25 +0900

    update W01 final

--- W01/C_programmers_60058.py
+++ W01/C_programmers_60058.py
@@ -1,1 +1,46 @@
-# 시간복잡도: 
+# 시간복잡도: O(N^2)
+
+def solution(p):
+    res = ''
+    if len(p) == 0 or isRight(p) == True: return p
+    else:
+        u, v = isBalance(p)
+        if isRight(u) == True:
+            res += u
+            res += solution(v)
+            return res
+        else:
+            res += "("
+            res += solution(v)
+            res += ")"
+            a = ""
+            for i in u[1:-1]:
+                if i == "(": a += ")"
+                else: a += "("
+            res += a
+            return res
+
+def isBalance(p):
+    l_cnt, r_cnt = 0, 0
+    for i in p:
+        if i == "(": l_cnt += 1
+        else: r_cnt += 1
+        if r_cnt != 0 and l_cnt == r_cnt:
+            idx = l_cnt + r_cnt
+            u, v = p[:idx], p[idx:]
+            return u, v
+
+def isRight(p):
+    li = []
+    for i in p:
+        if i == "(": li.append(i)
+        else:
+            if len(li) == 0: return False
+            li.pop()
+    if len(li) > 0: return False
+    else: return True
+
+"""
+- p가 빈 문자열이거나 올바른 문자열이면 p 그대로 return
+- 올바른 문자열은 먼저 res에 추가하고 올바르지 않은 문자열을 다시 solution 함수로 처리
+"""
--- W01/Readme.md
+++ W01/Readme.md
@@ -2,6 +2,7 @@
 
 ## 공통문제
 - [백준 11729 하노이 탑 이동 순서](https://www.acmicpc.net/problem/11729)
+    - [하노이 탑 정리](https://github.com/JiSuMun/New_TIL/blob/main/Algorithm/hanoi.md)
 - [프로그래머스 60058 괄호 변환](https://school.programmers.co.kr/learn/courses/30/lessons/60058?language=python3)
 
 ## 개인문제
