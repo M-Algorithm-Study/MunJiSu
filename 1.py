ad9c13ed23fad239c23c8c2624a018d3b1ff510d
JiSuMun <mjs3827@naver.com>
2023-09-04 02:30:57 +0900

    update W10

--- 1.py
+++ 1.py
@@ -1,16 +1,3 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-T = int(input())
-
-for t in range(1, T+1) :
-    N = int(input())
-
-    res = 1
-    for i in range(2, N+1) :
-        if i % 2 == 0 :
-            res -= i
-        else :
-            res += i
-
-    print("#{} {}".format(t, res))
--- W10/P_s_1986.py
+++ W10/P_s_1986.py
@@ -1,3 +1,18 @@
+# 시간복잡도: O(N)
+
 import sys
 sys.stdin = open("input_1986.txt", "r")
 
+T = int(input())
+
+for t in range(1, T+1) :
+    N = int(input())
+
+    res = 1
+    for i in range(2, N+1) :
+        if i % 2 == 0 :
+            res -= i
+        else :
+            res += i
+
+    print("#{} {}".format(t, res))
