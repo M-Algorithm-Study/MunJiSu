a98c5520c05811e5024eee4e90068aaa3aba9781
JiSuMun <mjs3827@naver.com>
2023-09-11 09:15:04 +0900

    update W11 14719

--- W11/C_b_14719.py
+++ W11/C_b_14719.py
@@ -1,2 +1,17 @@
-# 시간복잡도: 
+# 시간복잡도: O(W^2)
 
+import sys
+
+input = sys.stdin.readline
+
+H, W = map(int, input().split())
+height = list(map(int, input().split()))
+
+cnt = 0
+for i in range(1, W - 1):
+    left = max(height[:i])
+    right = max(height[i + 1 :])
+    low = min(left, right)
+    if height[i] < low:
+        cnt += low - height[i]
+print(cnt)
