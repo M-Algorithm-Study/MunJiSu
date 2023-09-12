e0d33d3e82f876c473bdccf904f2fff47ccb25f6
JiSuMun <mjs3827@naver.com>
2023-09-12 15:09:10 +0900

    update W11 swea 1859

--- W11/P_s_1859.py
+++ W11/P_s_1859.py
@@ -1,4 +1,18 @@
-# 시간복잡도: 
+# 시간복잡도: O(N)
 
 import sys
 sys.stdin = open("input_1859.txt", "r")
+
+T = int(input())
+for t in range(1, T + 1):
+    N = int(input())
+    nums = list(map(int, input().split()))
+    res = 0
+    price = 0
+    for num in nums[::-1]:
+        if num >= price:
+            price = num
+        else:
+            res += price - num
+
+    print("#{} {}".format(t, res))
