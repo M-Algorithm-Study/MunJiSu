68feef77bd976358d83260abcfd526f03c0326c8
JiSuMun <mjs3827@naver.com>
2023-10-21 01:04:39 +0900

    add W16 2565

---
+++ W16/C_b_2565.py
@@ -0,0 +1,24 @@
+# 시간복잡도: O(N^2)
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+cable = sorted([list(map(int, input().split())) for _ in range(n)])
+
+# 증가하지 않더라도 해당 순서만 포함된 수열이 있기 때문에 전체 초기값 1
+dp = [1] * n
+for i in range(n):
+    for j in range(i):
+        if cable[i][1] > cable[j][1]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(cable)
+print(dp)
+print(n - max(dp))
+
+"""
+A 전깃줄 순으로 정렬해서 다음 번호의 B 전깃줄이 이전 번호의 B 전깃줄 숫자보다 작으면 교차
+A 기준 정렬 => B 전깃줄 최대 증가 수열 찾기
+"""
--- W16/Readme.md
+++ W16/Readme.md
@@ -2,7 +2,7 @@
 
 ## 공통문제
 - [백준 14888 연산자 끼워넣기](https://www.acmicpc.net/problem/14888)
-<!-- - [백준 1463 1로 만들기](https://www.acmicpc.net/problem/1463) -->
+- [백준 2565 전깃줄](https://www.acmicpc.net/problem/2565)
 
 ## 개인문제
 <!-- - [SWEA 1984 중간 평균값 구하기](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5Pw_-KAdcDFAUq) -->
