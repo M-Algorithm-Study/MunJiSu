145f18cc260084e86ee622e7496cd275c09404be
JiSuMun <mjs3827@naver.com>
2023-11-10 11:23:22 +0900

    add W19 12939

---
+++ W19/P_p_12939.py
@@ -0,0 +1,10 @@
+# 시간복잡도: O(N)
+
+
+def solution(s):
+    answer = ""
+    nums = list(map(int, s.split()))
+    answer += str(min(nums))
+    answer += " "
+    answer += str(max(nums))
+    return answer
